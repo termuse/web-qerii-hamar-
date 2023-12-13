53ad8ea1f4e2c2ffef2b48f7e5b0d12cef607677
termuse <151167037+termuse@users.noreply.github.com>
2023-12-13 13:09:23 +0100

    Add files via upload

---
+++ voice ai/script.js
@@ -0,0 +1,8 @@
+    function copyText(){
+        navigator.clipboard.writeText
+        ("+37477167686")
+    } 
+        function copytext(){
+        navigator.clipboard.writeText
+        ("kahuyq_patverov89")
+    } 
